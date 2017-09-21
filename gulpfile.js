5c24f6e7c728cba9c0e114f4335893128c674e9a
Eddy <eharless@gmail.com>
2017-09-20 20:10:24 -0500

    initial

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,13 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function(){
-    console.log("yay!");
-});
 
-gulp.task('html', function(){
-    console.log("HTML fun here");
-});
 
-gulp.task('styles', function(){
-    gulp.src('./app/assets/Styles/styles.css')
-    .pipe(postcss([cssvars,autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task('watch', function(){
-    
-    watch('./app/index.html', function() {
-        gulp.start('html');
-    });
 
-    watch('./app/assets/styles/**/*.css', function() {
-        gulp.start('styles');
-    });
 
-});
-// 
+
+
+
+
+
